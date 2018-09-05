d1dce7b4e5f3e4e883a2cd8cdc15783ed108cd03
Marijn van Vliet <w.m.vanvliet@gmail.com>
2018-09-05 11:13:11 +0300

    Remove EOG and ECG in example

--- example.py
+++ example.py
@@ -4,17 +4,25 @@ import microstates
 from mne.datasets import sample
 fname = sample.data_path() + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
 raw = mne.io.read_raw_fif(fname, preload=True)
+events = mne.find_events(raw)
 raw.info['bads'] = ['MEG 2443', 'EEG 053']
-
 raw.set_eeg_reference('average')
+raw.pick_types(meg='mag', eeg=True, eog=True, ecg=True)
 raw.filter(1, 40)
+
+# Clean EOG with ICA
+ica = mne.preprocessing.ICA(0.99).fit(raw)
+bads_eog, _ = ica.find_bads_eog(raw)
+bads_ecg, _ = ica.find_bads_ecg(raw)
+ica.exclude = bads_eog[:2] + bads_ecg[:2]
+raw = ica.apply(raw)
 
 # Select sensor type
 # raw.pick_types(meg=False, eeg=True)
 raw.pick_types(meg='mag', eeg=False)
 
 # Segment the data in 6 microstates
-maps, segmentation = microstates.segment(raw.get_data(), n_states=6)
+maps, segmentation = microstates.segment(raw.get_data(), n_states=5)
 
 # Plot the topographic maps of the microstates and the segmentation
 microstates.plot_maps(maps, raw.info)
--- microstates.py
+++ microstates.py
@@ -13,6 +13,7 @@ import warnings
 import numpy as np
 from scipy.stats import zscore
 from scipy.signal import find_peaks
+from scipy.linalg import eigh
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 import mne
@@ -84,7 +85,7 @@ def segment(data, n_states=4, n_inits=10, max_iter=1000, thresh=1e-6,
     # min_peak_dist = 1 + int(round(min_peak_dist * raw.info['sfreq']))
 
     # Find peaks in the global field power (GFP)
-    gfp = np.sqrt(np.sum(data ** 2, axis=0))
+    gfp = data.std(axis=0)
     peaks, _ = find_peaks(gfp, distance=min_peak_dist)
     n_peaks = len(peaks)
 
@@ -134,7 +135,6 @@ def _mod_kmeans(data, n_states=4, n_inits=10, max_iter=1000, thresh=1e-6,
     """
     if not isinstance(random_state, np.random.RandomState):
         random_state = np.random.RandomState(random_state)
-
     n_channels, n_samples = data.shape
 
     # Cache this value for later
@@ -149,22 +149,27 @@ def _mod_kmeans(data, n_states=4, n_inits=10, max_iter=1000, thresh=1e-6,
     for iteration in range(max_iter):
         # Assign each sample to the best matching microstate
         activation = maps.dot(data)
-        segmentations = np.argmax(activation ** 2, axis=0)
+        segmentation = np.argmax(np.abs(activation), axis=0)
         # assigned_activations = np.choose(segmentations, all_activations)
 
         # Recompute the topographic maps of the microstates, based on the
         # samples that were assigned to each state.
         for state in range(n_states):
-            idx = (segmentations == state)
+            idx = (segmentation == state)
             if np.sum(idx) == 0:
                 warnings.warn('Some microstates are never activated')
                 maps[state] = 0
                 continue
+           
+            # Find largest eigenvector
+            # cov = data[:, idx].dot(data[:, idx].T)
+            # _, vec = eigh(cov, eigvals=(n_channels - 1, n_channels - 1))
+            # maps[state] = vec.ravel()
             maps[state] = data[:, idx].dot(activation[state, idx])
             maps[state] /= np.linalg.norm(maps[state])
 
         # Estimate residual noise
-        act_sum_sq = np.sum(np.sum(maps[segmentations].T * data, axis=0) ** 2)
+        act_sum_sq = np.sum(np.sum(maps[segmentation].T * data, axis=0) ** 2)
         residual = abs(data_sum_sq - act_sum_sq)
         residual /= float(n_samples * (n_channels - 1))
 
@@ -179,9 +184,9 @@ def _mod_kmeans(data, n_states=4, n_inits=10, max_iter=1000, thresh=1e-6,
 
     # Compute final microstate segmentations
     activation = maps.dot(data)
-    segmentations = np.argmax(activation ** 2, axis=0)
+    segmentation = np.argmax(activation ** 2, axis=0)
 
-    return maps, segmentations
+    return maps, segmentation
 
 
 def _corr_vectors(A, B, axis=0):
@@ -226,7 +231,7 @@ def plot_segmentation(segmentation, data, times):
     times : list of float
         The time-stamp for each sample.
     """
-    gfp = np.sqrt(np.sum(data ** 2, axis=0))
+    gfp = data.std(axis=0)
 
     n_states = len(np.unique(segmentation))
     plt.figure(figsize=(6 * np.ptp(times), 2))
